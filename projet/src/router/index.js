041f80d23dc5f66e995f8dc265e787f25044eec2
Léo BOUDER <leo.bouder@outlook.fr>
2025-05-07 16:38:39 -0400

    arboresence + router

---
+++ projet/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Reseau from '../views/Reseau.vue'
+import Statistiques from '../views/Statistiques.vue'
+import Connexion from '../views/Connexion.vue'
+import Inscription from '../views/Inscription.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/reseau', name: 'Reseau', component: Reseau },
+  { path: '/statistiques', name: 'Statistiques', component: Statistiques },
+  { path: '/connexion', name: 'Connexion', component: Connexion },
+  { path: '/inscription', name: 'Inscription', component: Inscription },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
