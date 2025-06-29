484db657c060b13df428e7f59e67c54c62410d09
amboura <josephfeghali02@gmail.com>
2025-06-29 18:55:45 -0400

    Ajout de l'endpoint compteurs - T4.2

--- projet/server/server.js
+++ projet/server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-require('./database');
+const db = require('./database');
+const { error } = require('console');
 
 const app = express();
 const PORT = 8000;
@@ -11,6 +12,29 @@ app.use(express.json());
 
 app.get('/gti525/v1/test', (req, res) =>{
     res.json({message: 'API GTI525 FONCTIONNEL'});
+});
+
+app.get('/gti525/v1/compteurs/:id', (req,res)=>{
+    const compteurId = req.params.id;
+    const debut = req.query.debut;
+    const fin = req.query.fin;
+    
+    let query = `SELECT * FROM comptage_velo WHERE id_compteur = ?`;
+    let params = [compteurId];
+
+    if(debut && fin){
+        query += ` AND date_heure BETWEEN ? AND ?`;
+        params.push(debut, fin);
+    }
+
+    db.all(query, params, (err, rows)=>{
+        if(err){
+            console.error(err);
+            res.status(500).json({error: "Erreur lors de la recupperation des donnees"});
+        }else{
+            res.json(rows);
+        }
+    });
 });
 
 app.listen(PORT, () =>{
